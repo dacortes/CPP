d4302b3556d813f3487f2facd42e3b0d681a233e
dacortes <dannyel.cortesr@gmail.com>
2024-01-09 18:12:33 +0100

    [IMP] to upper OK

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,13 +6,31 @@
 /*   By: dacortes </var/mail/dacortes>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 11:21:46 by dacortes          #+#    #+#             */
-/*   Updated: 2024/01/09 11:22:27 by dacortes         ###   ########.fr       */
+/*   Updated: 2024/01/09 18:11:36 by dacortes         ###   ########.fr       */
+//	std::ini
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <locale>
 
-int main(void)
+int main(int ac, char **av)
 {
-  return (printf("hello world!!!!\n") * 0);
+	std::string	str;
+
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int word = 1; word < ac; word++)
+		{
+			str = av[word];
+			for (long unsigned int	c = 0; c < str.length(); c++)
+				str[c] = toupper(av[word][c]);
+			std::cout << str;
+		}
+	}
+	std::cout << "\n";
+	return (0);
 }
